e513a4ac7f63b9df84a2c4e3283e5d9d48a13d30
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-09-06 16:54:39 +0200

    u8-d3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TopBar from "./components/TopBar";
-import AllTheBooks from "./components/AllTheBooks";
+/* import AllTheBooks from "./components/AllTheBooks"; */
+import Welcome from "./components/Welcome";
+import MyFooter from "./components/MyFooter";
+import SingleBook from "./components/SingleBook";
+import BookList from "./components/BookList";
 function App() {
   return (
     <div className="App">
       <TopBar brand="Libreria di React" />
-      <AllTheBooks />
+      <Welcome title="benvenuto" />
+      {/* <AllTheBooks /> */}
+      <SingleBook />
+      <BookList />
+      <MyFooter />
     </div>
   );
 }
--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import { Row, Col, Card } from "react-bootstrap";
-import library from "../data/books/scifi.json";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import library from "../data/books/romance.json";
 class AllTheBooks extends Component {
   state = {
     selectedLibro: null,
@@ -8,19 +8,21 @@ class AllTheBooks extends Component {
   render() {
     console.log("RENDER avvenuto", this.state);
     return (
-      <Row>
-        {library.map((book, index) => (
-          <Col md={4} className="mb-3" key={index}>
-            <Card>
-              <Card.Img variant="top" src={book.img} alt={book.title} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>{book.price}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <Container>
+        <Row>
+          {library.map((book, index) => (
+            <Col md={4} className="mb-3" key={index}>
+              <Card>
+                <Card.Img variant="top" src={book.img} alt={book.title} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.price}€</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     );
   }
 }
